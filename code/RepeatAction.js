12d73c3f2bfb679ec5162cd059d8c3b2809f7f2e
Van Boughner <van@viv.ai>
2019-09-29 16:08:03 -0700

    added concepts for voices

--- code/RepeatAction.js
+++ code/RepeatAction.js
@@ -1,3 +1,17 @@
-module.exports.function = function repeatAction (repeatInput) {
-  return repeatInput
+module.exports.function = function repeatAction (repeatInput, voiceStyle) {
+  const console = require("console")
+  const displayText = repeatInput
+
+  let speechText
+  if (voiceStyle) {
+    speechText = "<speak> <lang xml:lang=\"" + voiceStyle.locale + "\" voice=\"" + voiceStyle.profile + "\">" + repeatInput + " </lang></speak>"
+  } else {
+    speechText = repeatInput
+  }
+  console.log('speechText is', speechText)
+
+  return {
+    displayText: displayText,
+    speechText: speechText
+  }
 }
